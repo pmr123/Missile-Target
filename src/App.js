f10edebc65c3cae4622ab25da810e9b47f02e73f
dumbcoder7 <2017.aditya.raute@ves.ac.in>
2020-01-15 11:56:55 +0530

    Electron it is

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
-
-import Blog from './containers/Blog/Blog';
+// import Blog from './containers/Blog/Blog';
+import ThreeAnim from './components/ThreeAnim/ThreeAnim';
 
 class App extends Component {
+
+  
+
   render() {
     return (
       <div className="App">
-        <Blog />
+        <div>LOOK BELOW!!!</div>
+        <ThreeAnim />
+
       </div>
     );
   }
